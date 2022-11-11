55490d98729d8a9542cfc6e5cb743913c4197da7
jmoletteire <jmoletteire@ufl.edu>
2022-11-10 22:22:49 -0500

    updated html and css elements, adding dynamic sliders.

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,8 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerText = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  output.innerText = this.value;
 }
